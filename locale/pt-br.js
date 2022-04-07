ca1e1b44956852df07c09424234af791abaec0f8
laurojunior <laurolimajunior@gmail.com>
2022-04-07 09:39:20 -0300

    Update pt-br.js

--- locale/pt-br.js
+++ locale/pt-br.js
@@ -1,1 +1,76 @@
-
+export default {
+	home: {
+		section: {
+			list: {
+				title: 'Listas recomendadas'
+			},
+			newMaterial: {
+				title: 'Novos materiais',
+				subtitle: 'recomendados para você'
+			},
+			savedMaterial: {
+				title: 'Últimos {materialCategory} que você salvou',
+				materials: 'materiais'
+			},
+			trendingMaterial: {
+				title: 'Materiais em alta',
+				subtitle: 'Os mais vistos, recomendados para você'
+			},
+			producerMaterial: {
+				title: 'Materiais de especialistas',
+				subtitle: 'porque você segue esses perfis'
+			},
+			reasonFollowing: 'porque você segue essa disciplina',
+			reasonNotFollowing: 'porque você acessou materiais relacionados',
+			more: 'Ver mais',
+			empty: {
+				noCategory: {
+					title: 'Receba recomendações com seus interesses de estudo',
+					subtitle: 'Siga disciplinas e receba indicações com os melhores materiais para você',
+					buttonSubjects: 'Seguir disciplinas'
+				},
+				title: 'Estamos sem recomendações para você agora',
+				subtitle: 'Você pode buscar outros materiais ou ver todos os conteúdos clicando abaixo',
+				buttonFeminine: 'Ver todas as {materialCategory}',
+				buttonMasculine: 'Ver todos os {materialCategory}'
+			},
+			users: {
+				title: 'Perfis recomendados'
+			},
+			subjects: {
+				title: 'Disciplinas recomendadas para você'
+			},
+			continueStudying: {
+				title: 'Continue estudando'
+			}
+		},
+		all: {
+			name: 'Tudo',
+			title: 'Home '
+		},
+		exam: {
+			name: 'Provas',
+			title: 'Provas '
+		},
+		assignment: {
+			name: 'Trabalhos',
+			title: 'Trabalhos '
+		},
+		summary: {
+			name: 'Resumos',
+			title: 'Resumos '
+		},
+		homework: {
+			name: 'Exercícios',
+			title: 'Exercícios '
+		},
+		other: {
+			name: 'Outros',
+			title: 'Outros '
+		},
+		lecture: {
+			name: 'Aulas',
+			title: 'Aulas '
+		}
+	}
+};
